d1c5e08dbaa26b7406fd56adf34c1230cc4c5c64
Ollivier Robert <roberto@keltia.net>
2015-05-29 12:30:17 +0200

    Use flag. Subscribe to topics.

--- surv-export/main.go
+++ surv-export/main.go
@@ -7,23 +7,25 @@ package main
 import (
 	"../config"
 	"../surv"
+	"flag"
 	"fmt"
 	"os"
 	"path/filepath"
-
-	"github.com/codegangsta/cli"
+	"text/tabwriter"
 )
 
 var (
 	RcFile = filepath.Join(os.Getenv("HOME"), ".surveillance", "config.yml")
+
+	Feeds = map[string]string{
+		"AsterixJSON": "feed_json",
+		"AsterixXML": "feed_xml",
+		"AsterixJSONgzipped": "feed_jsongz",
+	}
 )
 
 func main() {
-	app := cli.NewApp()
-	app.Name = "surv-export"
-	app.Author = "Ollivier Robert"
-	app.Version = "0.0.1"
-	app.Usage = "surv-export"
+	var feeds []string
 
 	c, err := config.LoadConfig(RcFile)
 	if err != nil {
@@ -32,7 +34,24 @@ func main() {
 	fmt.Println(c.Dests)
 	fmt.Println(c.Default, c.Dests[c.Default])
 
+	flag.Parse()
 	client, err := surv.NewClient(c)
+
+	// Look for feed names on CLI
+	for _, tn := range flag.Args {
+		if Feeds[tn] != "" {
+			feeds = append(feeds, tn)
+		}
+	}
+	for _, tn := range feeds {
+		// server := NewServer(tn
+		unsubFn, err := client.Subscribe(tn, Feeds[tn])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error subscribing to %n: %v", tn, err)
+		}
+		topic := surv.Topic{Started: true, Address: unsubFn}
+		client.Topics[tn] = topic
+	}
 	for name, topic := range(client.Topics) {
 		fmt.Printf("Topic: %s Bytes: %ld Pkts: %d", name, topic.Bytes, topic.Pkts)
 	}
