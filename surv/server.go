1f60b5b80bb6a030db287e1769cd2fc2fb799e19
Ollivier Robert <roberto@keltia.net>
2015-06-02 17:24:33 +0200

    Wrap handleNotify() as an http.Handler
    - Use makeHandler to wrap handleNotify
    - Properly init and start the server
    - Add a lot of logging
    - Add getFeedName() from URI
    - Unmarshall the XMLpayload from SOAP

--- surv/server.go
+++ surv/server.go
@@ -15,43 +15,70 @@ import (
 
 var survClient *Client
 
-func handleNotify(w http.ResponseWriter, req *http.Request) {
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		//defer func() {
+		//	if e, ok := recover().(error); ok {
+		//		log.Println(e)
+		//		http.Error(w, e.Error(), http.StatusInternalServerError)
+		//		return
+		//	}
+		//}()
+		title := r.URL.Path
+		log.Println(title)
+		fn(w, r, title)
+	}
+}
+
+// Get the last component of URI
+func getFeedName(url string) string {
+	// FIXME
+	parts := strings.Split(url, "/")
+	return parts[len(parts) - 1]
+
+}
+
+func handleNotify(w http.ResponseWriter, req *http.Request, url string) {
+	log.Printf("%s %s %s", req.RemoteAddr, req.Method, req.URL)
+	log.Println(req)
 	//
 	// body is an XML SOAP
 	//
-	log.Println("In handleNotify")
-	body, err := ioutil.ReadAll(req.Body)
-	defer req.Body.Close()
+	if req.Method == "POST" {
+		log.Println("In handleNotify")
+		body, err := ioutil.ReadAll(req.Body)
+		defer req.Body.Close()
 
-	notify := SurvData{}
-	err = xml.Unmarshal(body, &notify)
-	if err != nil {
-		real := fmt.Sprintf("Error: %v", err)
-		http.Error(w, real, 500)
+		last := getFeedName(url)
+		log.Println("Request is", last)
+
+		notify := &SurvData{}
+		err = xml.Unmarshal(body, notify)
+		if err != nil {
+			real := fmt.Sprintf("Error: %v", err)
+			log.Println(real)
+			//http.Error(w, real, 500)
+			return
+		}
+
+		//topicList := survClient.Topics
+		//log.Println(topicList[last])
+
+		//topicList[last].Bytes += int64(len(notify.Body.Notify.Message.Payload.Data))
+		//topicList[last].Pkts++
+
+		log.Printf("%+v", string(notify.Body.Notify.NotifyMsg.Message.Payload.Text))
+	} else {
+		http.NotFound(w, req)
 	}
-	// FIXME
-	pathInfo := req.Header.Get("Path-Info")
-	log.Println(pathInfo)
-	parts := strings.Split(pathInfo, "/")
-	last := parts[len(parts) - 1]
-	log.Println(last)
-
-	topicList := survClient.Topics
-	log.Println(topicList[last])
-
-	//topicList[last].Bytes += int64(len(notify.Body.Notify.Message.Payload.Data))
-	//topicList[last].Pkts++
-
-	fmt.Println(notify.Body.Notify.Message.Payload.Data)
 }
 
-func ServerStart(cl *Client, feeds []string) {
-	survClient = cl
+func ServerStart(cl *Client, feeds map[string]string) {
 	server := http.NewServeMux()
-	for _, feed := range feeds {
-		log.Println("Setting handler for "+feed)
-		server.HandleFunc(feed, handleNotify)
+	for name, feed := range feeds {
+		log.Printf("Setting handler for %s as /%s\n", name, feed)
+		server.HandleFunc("/" + feed, makeHandler(handleNotify))
 	}
-	log.Println("Serving...")
-	log.Fatal(http.ListenAndServe(":"+cl.config.Port, nil))
+	log.Println("Serving", feeds)
+	log.Fatal(http.ListenAndServe(":"+cl.Config.Port, server))
 }
