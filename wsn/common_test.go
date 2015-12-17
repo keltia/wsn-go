106114efa0afdf3b9099b781ac73a41a8a40f9e6
Ollivier Robert <roberto@keltia.net>
2015-12-17 14:01:23 +0100

    Rename test for consistency.

--- wsn/common_test.go
+++ wsn/common_test.go
@@ -7,7 +7,7 @@ import (
 
 var common_config = config.Config{Proto: "http", Site: "example.com", Port: 666, Endpoint: "foo"}
 
-func TestGenerateURL(t *testing.T) {
+func TestGenerateEndpoint(t *testing.T) {
 	url := generateEndpoint(&common_config)
 	if url != "http://example.com:666/foo" {
 		t.Errorf("Error: bad format %s for %v\n", url, common_config)
