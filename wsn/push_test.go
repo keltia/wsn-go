1dd489e365c77a8f91f47ed20babb3a080be9ee7
Ollivier Robert <roberto@keltia.net>
2015-12-22 00:47:43 +0100

    Add tests for NewPushClient().

---
+++ wsn/push_test.go
@@ -0,0 +1,46 @@
+// Copyright 2015 Ollivier Robert for EUROCONTROL  All rights reserved
+
+package wsn
+
+import (
+	"testing"
+	"fmt"
+	"wsn-go/config"
+)
+
+var _topics TopicList = TopicList{}
+
+func TestNewPushClient(t *testing.T) {
+	// Load our stuff
+	config, err :=  config.LoadConfig("../config/config.toml")
+	if err != nil {
+		t.Errorf("Error loading config: %v", err)
+	}
+	client := NewPushClient(config)
+
+	// Check various fields
+
+	// Public
+	t.Log("  Public fields")
+	if len(client.List) != 0 {
+		t.Errorf("List is not correctly set: %s", client.List)
+	}
+	if client.Timeout != int64(-1) {
+		t.Errorf("Timeout is not correctly set: %s", client.Timeout)
+	}
+
+	// Private
+	t.Log("  Private fields")
+	if client.base != config.Base {
+		t.Errorf("base is not correctly set: %s", client.base)
+	}
+	if client.target != fmt.Sprintf("%s:%d/%s", config.Target, config.Port, config.Broker) {
+		t.Errorf("target is not correctly set: %s", client.target)
+	}
+	if client.port != config.Port {
+		t.Errorf("port is not correctly set: %s", client.port)
+	}
+	if client.verbose != false {
+		t.Errorf("verbose is not correctly set: %s", client.verbose)
+	}
+}
