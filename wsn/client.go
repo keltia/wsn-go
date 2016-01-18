047860ffa2643a50da09d448bbc286f5f92a27b9
Ollivier Robert <roberto@keltia.net>
2016-01-18 14:50:31 +0100

    Move errors into their own file here too.

--- wsn/client.go
+++ wsn/client.go
@@ -22,25 +22,3 @@ type Client interface {
 	Stop() error
 }
 
-// Error list
-
-// Channel errors
-var ErrNoOutputChannel = errors.New("Output channel not opened")
-
-// Topic errors
-var ErrTopicNotFound = errors.New("Topic not found")
-
-// Can't add an existing topic
-var ErrTopicAlreadyExist = errors.New("Topic already exist!")
-
-// Can't subscribe more than once
-var ErrTopicAlreadySubscribed = errors.New("Topic already subscribed!")
-
-// Pull point creation error
-var ErrCreatingPullPoint = errors.New("Can not create pull point")
-
-// Pull point destruction errors
-var ErrDestroyingPullPoint = errors.New("Can not destroy pull point")
-
-// Templating error
-var ErrCantCreateTemplate = errors.New("Can not instanciate template")
