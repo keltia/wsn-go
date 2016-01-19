d1c932ddfa8477f2340e3a817525b48833cab9bd
Ollivier Robert <roberto@keltia.net>
2016-01-19 17:21:04 +0100

    Add createPullPoint(), make it tied to the PulClient interface.

--- wsn/pull_internal.go
+++ wsn/pull_internal.go
@@ -2,20 +2,31 @@
 
 package wsn
 
-import "fmt"
-
-// generateURL generates an URL on the target site
-func (c *PullClient) generateURL(endPoint string) string {
-	return fmt.Sprintf("%s:%d/%s", c.target, c.port, endPoint)
-}
+import (
+	"encoding/xml"
+	"wsn-go/soap"
+)
 
 // createPullPoint create a Pull point on the broker that will be used to subscribe
 // topics.
-func createPullPoint() (pullPt string, err error) {
-	if false {
-		err = ErrCreatingPullPoint
+func (c *PullClient) createPullPoint() (pullPt string, err error) {
+
+	// Pull point should not exist at this stage
+	if c.PullPt == "" {
+		soapReq, err := soap.NewRequest(soap.CREATEPULLPOINT, soap.SubVars{})
+		if err != nil {
+			return "", ErrCreatingPullPoint
+		}
+		answer, err := soapReq.Send(c.target)
+		if err != nil {
+			var res *CPPBody
+
+			err = xml.Unmarshal(answer, res)
+		}
+	} else {
+		return "", ErrPullPtAlreadyPresent
 	}
-	return
+	return c.PullPt, nil
 }
 
 // destroyPullPoint de-registers the pull point to avoid hogging resources on the broker
