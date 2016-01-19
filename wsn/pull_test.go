c3a373a7b68b36ae83368618729388a78859f3cf
Ollivier Robert <roberto@keltia.net>
2016-01-19 13:32:32 +0100

    First tests for PullClient.

---
+++ wsn/pull_test.go
@@ -0,0 +1,72 @@
+// Copyright 2015 Ollivier Robert for EUROCONTROL  All rights reserved
+
+package wsn
+
+import (
+	"fmt"
+	"testing"
+
+	"wsn-go/config"
+)
+
+func TestNewPullClient(t *testing.T) {
+	// Load our stuff
+	config, err := config.LoadConfig("../config/config.toml")
+	if err != nil {
+		t.Errorf("Error loading config: %v", err)
+	}
+	client := NewPullClient(config)
+
+	// Check various fields
+
+	// Public
+	if len(client.List) != 0 {
+		t.Errorf("List is not correctly set: %s", client.List)
+	}
+	if client.Timeout != int64(-1) {
+		t.Errorf("Timeout is not correctly set: %s", client.Timeout)
+	}
+
+	// Private
+	if client.base != config.Base {
+		t.Errorf("base is not correctly set: %s", client.base)
+	}
+	if client.target != fmt.Sprintf("%s:%d/%s", config.Target, config.Port, config.Broker) {
+		t.Errorf("target is not correctly set: %s", client.target)
+	}
+	if client.port != config.Port {
+		t.Errorf("port is not correctly set: %s", client.port)
+	}
+	if client.verbose != false {
+		t.Errorf("verbose is not correctly set: %s", client.verbose)
+	}
+
+}
+
+func TestPullType(t *testing.T) {
+	// Load our stuff
+	config, err := config.LoadConfig("../config/config.toml")
+	if err != nil {
+		t.Errorf("Error loading config: %v", err)
+	}
+	client := NewPullClient(config)
+
+	if client.Type() != MODE_PULL {
+		t.Errorf("Wrong type for %v", client)
+	}
+}
+
+func TestPullSetVerbose(t *testing.T) {
+	// Load our stuff
+	config, err := config.LoadConfig("../config/config.toml")
+	if err != nil {
+		t.Errorf("Error loading config: %v", err)
+	}
+	client := NewPullClient(config)
+
+	client.SetVerbose()
+	if !client.verbose {
+		t.Errorf("Error setting verbose mode: %v", client.verbose)
+	}
+}
+
